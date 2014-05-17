0948d5a1f22ce15660c31e7ab498da332af90713
Robotics <Robotics@home>
2014-05-16 20:26:27 -0400

    websockets work

--- server.py
+++ server.py
@@ -1,29 +1,82 @@
 #!/usr/bin/env python3
 
 from tornado.options import options, define, parse_command_line
-from tornado import web
+from tornado import web, websocket
 import tornado.ioloop
 import tornado.httpserver
 import os
+from collections import defaultdict
 
-define("port", type=int, default=8078)
+define("port", type=int, default=8888)
+
+template_context = {"pid":os.getpid()}
 
 class ConfigHandler(web.RequestHandler):
     def get(self):
         self.render("static/config", pid=os.getpid())
 
+class StaticTemplateHandler(web.RequestHandler):
+    def get(self, path):
+        # check real file name:
+        # if folder, use 1
+        # if js, use other
+        print(path)
+        self.render("static/index".format(path), **template_context)
+
+class ReceiveHandler(web.RequestHandler):
+    def post(self):
+        key = self.get_argument("key", "???")
+        value = self.get_argument("value", "???")
+        print(key, "=>", value)
+
+# todo: convert into engine
+pagelist = defaultdict(list)
+
+class Interface():
+    def __init__(self, push):
+        self.push = push
+        # default callback is echo
+        self.callback = lambda x:self.push(x[0],x[1])
+    def pull(self,callback):
+        self.callback = callback
+
+class SocketHandler(websocket.WebSocketHandler):
+    path = None
+    interface = None
+    def write_pair(self, key, val):
+        self.write_message("{}={}".format(key, val))
+    def on_message(self, message):
+        print(message)
+        if self.path is None:
+            self.path = message
+            self.interface = Interface(self.write_pair)
+            pagelist[self.path].append(self.interface)
+        else:
+            for x in pagelist[self.path]:
+                x.callback(message.split("="))
+
+    def on_close(self):
+        pagelist[self.path].remove(self.interface)
+    def open(self):
+        pass
+
 def main():
     parse_command_line()
     app = web.Application(
     [
-        # TODO: produce a "folderhandler" that filters out the index path,
-        # and links the /view index properly
+        ("/socket", SocketHandler),
+
+
+
         ("/config", ConfigHandler),
-        ("/()", web.StaticFileHandler, {"path":"static/index"}),
+        ("/push", ReceiveHandler),
+        ("/index", web.ErrorHandler, {"status_code":404}),
+        ("/()", StaticTemplateHandler),
         ("/view()", web.StaticFileHandler, {"path":"static/view/index"}),
         ("/input()", web.StaticFileHandler, {"path":"static/input/index"}),
-        ("/index", web.ErrorHandler, {"status_code":404}),
-        ("/(.*)", web.StaticFileHandler, {"path": "static"})
+
+
+        ("/(.*)", web.StaticFileHandler,  {"path": "static"})
     ])
     server = tornado.httpserver.HTTPServer(app)
     server.listen(options.port)
---
+++ static/js/pushpull.js
@@ -0,0 +1,37 @@
+"use strict"
+
+alert = (function _() {
+    var pushqueue = [];
+    var available = false;
+    var pulls = {}
+
+    var socket = new WebSocket("ws://localhost:8888/socket");
+    socket.onopen = function () {
+        socket.send(window.location.pathname);
+        while (pushqueue.length > 0) {
+            socket.send(pushqueue.pop())
+        }
+        available = true;
+    }
+    socket.onmessage = function(e) {
+        var d = e.data.split("=");
+        if (d[0] in pulls) {
+            pulls[d[0]](d[1]);
+        }
+    }
+
+    window.push = function push(key, value) {
+        pushqueue.push(String(key) + "=" + String(value))
+        if (available) {
+            while (pushqueue.length > 0) {
+                socket.send(pushqueue.pop())
+            }
+        }
+    }
+
+    window.pull = function pull(key, callback) {
+        pulls[key] = callback;
+    }
+
+    return alert
+}).call()
